db9dc03ca27760ecdb8ee3458fa7e7f078de352e
Ben Foxall <ben@whiteoctober.co.uk>
2014-05-22 18:41:04 +0100

    request page

--- js/lastfm-export.js
+++ js/lastfm-export.js
@@ -1,20 +1,42 @@
 var username = 'benjaminf',
   key = 'c2899b0774a2eda7769be6eefddd94b6';
 
-reqwest({
-  url:"http://ws.audioscrobbler.com/2.0/",
-  data: {
-    method:'user.getrecenttracks',
-    format:'json',
-    user:username,
-    api_key:key,
-    limit:200,
-    page:0
-  }
-})
-.then(function (resp) {
-  console.log(resp)
-})
+// reqwest({
+//   url:"http://ws.audioscrobbler.com/2.0/",
+//   data: {
+//     method:'user.getrecenttracks',
+//     format:'json',
+//     user:username,
+//     api_key:key,
+//     limit:200,
+//     page:0
+//   }
+// })
+// .then(function (resp) {
+//   console.log(resp)
+// })
+
+
+function requestPage(key, user, page){
+
+  return reqwest({
+    url:"http://ws.audioscrobbler.com/2.0/",
+    data: {
+      method:'user.getrecenttracks',
+      format:'json',
+      user:user,
+      api_key:key,
+      limit:200,
+      page:page
+    }
+  })
+
+}
+
+function extractTracks(json){
+  return json.recenttracks.track
+}
+
 
 
 
